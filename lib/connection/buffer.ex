bbead4df3570fc87f3b0a604eb3105b0c53ea198
Victor Gaiva <13839490+VictorGaiva@users.noreply.github.com>
2022-08-13 23:44:39 -0300

    Simplifying Buffer decoding logic

--- lib/connection/buffer.ex
+++ lib/connection/buffer.ex
@@ -1,6 +1,7 @@
 defmodule RabbitMQStream.Connection.Buffer do
   alias RabbitMQStream.{Message, Connection}
   alias Message.{Request, Response, Decoder, Command}
+  import Bitwise
 
   def handle_decode(%Connection{} = conn, data) when is_binary(data) do
     messages = decode!(data)
@@ -15,35 +16,19 @@ defmodule RabbitMQStream.Connection.Buffer do
   defp decode!(buffer) when is_binary(buffer) do
     Stream.cycle([0])
     |> Enum.reduce_while({buffer, []}, fn
-      _, {<<size::unsigned-integer-size(32), _::binary-size(size)>> = buffer, acc} ->
+      _, {<<size::unsigned-integer-size(32), buffer::binary-size(size)>>, acc} ->
         {:halt, acc ++ [do_decode!(buffer)]}
 
-      _, {<<size::unsigned-integer-size(32), _::binary>> = buffer, acc} ->
-        size = size + 4
-
-        <<buffer::binary-size(size), rest::binary>> = buffer
-
+      _, {<<size::unsigned-integer-size(32), buffer::binary-size(size), rest::binary>>, acc} ->
         {:cont, {rest, acc ++ [do_decode!(buffer)]}}
     end)
   end
 
-  defp do_decode!(<<size::unsigned-integer-size(32), buffer::binary-size(size)>>) do
-    <<
-      flag::1,
-      key::bits-size(15),
-      version::unsigned-integer-size(16),
-      buffer::binary
-    >> = buffer
-
-    <<key::unsigned-integer-size(16)>> = <<0b0::1, key::bits>>
-    command = Command.to_atom(key)
-
-    case flag do
-      0b1 ->
-        %Response{version: version, command: command}
-
-      0b0 ->
-        %Request{version: version, command: command}
+  defp do_decode!(<<key::integer-size(16), version::unsigned-integer-size(16), buffer::binary>>) do
+    if band(key, 32768) > 0 do
+      %Response{version: version, command: Command.to_atom(band(key, 32767))}
+    else
+      %Request{version: version, command: Command.to_atom(band(key, 32767))}
     end
     |> Decoder.decode!(buffer)
   end
