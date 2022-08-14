29925e01e109181ec5077fd36a732a34286f2bf5
Victor Gaiva <13839490+VictorGaiva@users.noreply.github.com>
2022-08-14 18:46:14 -0300

    Added osiris  dependency

--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule RabbitMQStream.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.28.4", only: :dev, runtime: false},
-      {:rabbitmq_stream_common, github: "rabbitmq/rabbitmq-server", subdir: "deps/rabbitmq_stream_common"}
+      {:rabbitmq_stream_common,
+       github: "rabbitmq/rabbitmq-server", subdir: "deps/rabbitmq_stream_common"},
+      {:osiris, github: "rabbitmq/osiris", manager: :make}
     ]
   end
 
