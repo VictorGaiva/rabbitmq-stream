2a69507a3fe6474143cfffae86192ddb2295de44
Victor Gaiva <13839490+VictorGaiva@users.noreply.github.com>
2024-02-10 01:42:40 -0300

    test: Adds failing test for partitions creation behavior

--- test/super_stream_test.exs
+++ test/super_stream_test.exs
@@ -71,33 +71,63 @@ defmodule RabbitMQStreamTest.SuperStream do
 
   @tag :v3_13
   test "should create and delete a super_stream", %{conn: conn} do
-    RabbitMQStream.Connection.delete_super_stream(conn, "test")
+    RabbitMQStream.Connection.delete_super_stream(conn, "transactions")
 
     :ok =
-      RabbitMQStream.Connection.create_super_stream(conn, "test",
-        "test-0": 0,
-        "test-1": 1,
-        "test-2": 2
+      RabbitMQStream.Connection.create_super_stream(conn, "transactions",
+        "test-foo": "A",
+        "test-bar": "B",
+        "test-baz": "C"
       )
 
-    {:ok, %{streams: ["test-0"]}} = RabbitMQStream.Connection.route(conn, "0", "test")
-    {:ok, %{streams: ["test-1"]}} = RabbitMQStream.Connection.route(conn, "1", "test")
-    {:ok, %{streams: ["test-2"]}} = RabbitMQStream.Connection.route(conn, "2", "test")
+    {:ok, %{streams: ["test-foo"]}} = RabbitMQStream.Connection.route(conn, "A", "transactions")
+    {:ok, %{streams: ["test-bar"]}} = RabbitMQStream.Connection.route(conn, "B", "transactions")
+    {:ok, %{streams: ["test-baz"]}} = RabbitMQStream.Connection.route(conn, "C", "transactions")
 
-    {:ok, %{streams: streams}} = RabbitMQStream.Connection.partitions(conn, "test")
+    {:ok, %{streams: streams}} = RabbitMQStream.Connection.partitions(conn, "transactions")
 
-    assert Enum.all?(streams, fn stream -> stream in ["test-0", "test-1", "test-2"] end)
+    assert Enum.all?(streams, fn stream -> stream in ["test-foo", "test-bar", "test-baz"] end)
 
-    :ok = RabbitMQStream.Connection.delete_super_stream(conn, "test")
+    for consumer <- [SuperConsumer1, SuperConsumer2, SuperConsumer3] do
+      {:ok, conn} = RabbitMQStream.Connection.start_link(host: "localhost", vhost: "/")
+      :ok = RabbitMQStream.Connection.connect(conn)
+
+      {:ok, _} =
+        consumer.start_link(
+          connection: conn,
+          super_stream: "invoices",
+          private: self()
+        )
+    end
+
+    # We wait a bit to guarantee that the consumers are ready
+    Process.sleep(500)
+
+    :ok = SuperProducer.publish("1")
+    :ok = SuperProducer.publish("12")
+    :ok = SuperProducer.publish("123")
+
+    msgs =
+      for _ <- 1..3 do
+        receive do
+          msg -> msg
+        after
+          500 -> :timeout
+        end
+      end
+
+    # Process.sleep(60_000)
+    assert SuperConsumer1 in msgs
+    assert SuperConsumer2 in msgs
+    assert SuperConsumer3 in msgs
+
+    :ok = RabbitMQStream.Connection.delete_super_stream(conn, "transactions")
   end
 
   @tag :v3_11
   @tag :v3_12
   @tag :v3_13
-  test "should create super streams" do
-    {:ok, conn} = RabbitMQStream.Connection.start_link(host: "localhost", vhost: "/")
-    :ok = RabbitMQStream.Connection.connect(conn)
-
+  test "should create super streams", %{conn: conn} do
     {:ok, %{streams: streams}} =
       RabbitMQStream.Connection.query_metadata(conn, ["invoices-0", "invoices-1", "invoices-2"])
 
@@ -105,35 +135,17 @@ defmodule RabbitMQStreamTest.SuperStream do
       raise "SuperStream streams were not found. Please ensure you've created the \"invoices\" SuperStream with 3 partitions using RabbitMQ CLI or Management UI before running this test."
     end
 
-    {:ok, _} =
-      SuperConsumer1.start_link(
-        connection: conn,
-        super_stream: "invoices",
-        private: self()
-      )
+    for consumer <- [SuperConsumer1, SuperConsumer2, SuperConsumer3] do
+      {:ok, conn} = RabbitMQStream.Connection.start_link(host: "localhost", vhost: "/")
+      :ok = RabbitMQStream.Connection.connect(conn)
 
-    {:ok, conn} = RabbitMQStream.Connection.start_link(host: "localhost", vhost: "/")
-    :ok = RabbitMQStream.Connection.connect(conn)
-
-    {:ok, _} =
-      SuperConsumer2.start_link(
-        connection: conn,
-        super_stream: "invoices",
-        private: self()
-      )
-
-    {:ok, conn} = RabbitMQStream.Connection.start_link(host: "localhost", vhost: "/")
-    :ok = RabbitMQStream.Connection.connect(conn)
-
-    {:ok, _} =
-      SuperConsumer3.start_link(
-        connection: conn,
-        super_stream: "invoices",
-        private: self()
-      )
-
-    {:ok, conn} = RabbitMQStream.Connection.start_link(host: "localhost", vhost: "/")
-    :ok = RabbitMQStream.Connection.connect(conn)
+      {:ok, _} =
+        consumer.start_link(
+          connection: conn,
+          super_stream: "invoices",
+          private: self()
+        )
+    end
 
     {:ok, _} =
       SuperProducer.start_link(
